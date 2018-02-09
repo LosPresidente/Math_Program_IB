aa1dee055318d7cef68d6cfefa646d8d92e1f5bc
LosPresidente <ibilek1@buffs.wtamu.edu>
2018-02-09 17:52:35 -0600

    Add project files.

---
+++ MathProgram/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MathProgram.Models;
+
+namespace MathProgram.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            Repository.AddResponse(guestResponse);
+            return View("Result", guestResponse);
+        }
+    }
+}
---
+++ MathProgram/Models/GuestResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MathProgram.Models
+{
+    public class GuestResponse
+    {
+
+        //this will actually be the user input of # >> only 2# ( public int first {get;set;} and public int second {get;set;}
+        public int First {get;set;}
+        public int Second {get;set;}
+        public string Operation {get;set;}
+        public bool? WillAttend  {get;set;}
+
+    }
+}
